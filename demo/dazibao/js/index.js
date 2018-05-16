a0be9188c64ca40b05acc2cbbd9660803622869e
duyangsir <55644146@qq.com>
2018-05-16 14:22:56 +0800

    111

--- demo/dazibao/js/index.js
+++ demo/dazibao/js/index.js
@@ -3,7 +3,23 @@ var app = new Vue({
   data: {
     // 使用说明
     explainDialog: false,
+    runBox: false,
+    begin: true,
     // 展示内容
-    textarea:'',
+    textarea: '',
+    textColor: '#000000',
+    bgColor: '#ffffff',
+    innerheight: '',
+    innerwidth: '',
+    speed: 50
+  },
+  mounted() {
+    this.innerheight = window.innerHeight
+    this.innerwidth = window.innerWidth
+  },
+  methods: {
+    textareaChange() {
+      this.begin = this.textarea.length === 0
+    }
   }
 })
--- demo/dazibao/js/run.js
+++ demo/dazibao/js/run.js
@@ -1,8 +1,14 @@
 Vue.component('run-box', {
+  props:['data'],
   data() {
     return {
       count: 0
     }
   },
-  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+  template: '<canvas   id="canvas" width="10" height="10"></canvas>',
+  mounted() {
+    this.innerheight = window.innerHeight
+    this.innerwidth = window.innerWidth
+    console.log(this.innerheight, this.innerwidth,this.data)
+  }
 })
